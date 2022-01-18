011dc2e3fe813119d53e753895e2644a27f1c296
Paiman-Rasoli <paimanrasoli789@gmail.com>
2022-01-18 21:20:03 +0430

    adding cookie-parser in app middleware

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express, { Application, Request, Response } from "express";
 import path from "path";
 const app: Application = express();
 const PORT = process.env.PORT || 9000;
-var colors = require("colors");
+let colors = require("colors");
 let log: Function = console.log;
+let cookieParser = require("cookie-parser");
 
 // Accept json / urlencoded or request
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 // view engines
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
