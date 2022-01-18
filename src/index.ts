e9694ecdc0990c41cac7a5d6c730e5b2bd2a93e4
Paiman-Rasoli <paimanrasoli789@gmail.com>
2022-01-18 16:26:05 +0430

    enabling ejs and creating index page

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import path from "path";
 const app: Application = express();
 const PORT = process.env.PORT || 9000;
 var colors = require("colors");
@@ -7,7 +8,9 @@ let log: Function = console.log;
 // Accept json / urlencoded or request
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+// view engines
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 app.use("/", require("./routes/main"));
 // Run the app and you will see the cb.
 app.listen(PORT, (): void => {
--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const routes: Router = Router();
 routes.get("/", (req, res) => {
-  res.send("Hi");
+  res.render("index.ejs", { name: "paiman" });
 });
 module.exports = routes;
 // module
