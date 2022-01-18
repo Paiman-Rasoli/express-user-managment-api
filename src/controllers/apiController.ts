8d1aabeeae19d441ef1abd243ac810de6ca74174
Paiman-Rasoli <paimanrasoli789@gmail.com>
2022-01-18 21:25:32 +0430

    check password hashed with plain password

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { users } from "../data/users";
+const bcrypt = require("bcryptjs");
 
 export class ApiController {
   req: Request;
@@ -12,7 +13,11 @@ export class ApiController {
     const { email, password } = this.req.body;
     const res = users.filter((item) => item.email === email);
     if (res.length > 0) {
-      return this.res.status(200).json({ email: email, pass: password });
+      // check plain password with hashed password
+      if (bcrypt.compareSync(password, res[0].password)) {
+        return this.res.status(200).json({ email: email, pass: password });
+      }
+      return this.res.status(400).json({ msg: "Invalid credentials!" });
     }
     return this.res.status(400).json({ msg: "User Not Found!" });
   }
